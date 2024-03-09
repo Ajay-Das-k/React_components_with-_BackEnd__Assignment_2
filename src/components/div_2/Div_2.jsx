da27632c1047072b3c19706a6ca3c53d16bb1c06
Ajay-Das-k <ajtechhub01@gmail.com>
2024-03-09 18:17:51 +0530

    refined Code

--- src/components/div_2/Div_2.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Div_2() {
-  return (
-    <div>Div_2</div>
-  )
-}
-
-export default Div_2
--- src/components/div_3/Div_3.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import DisplayData from "../dataDisplay/DisplayData"
-function Div_3() {
-  return (
-    <div><DisplayData></DisplayData></div>
-  )
-}
-
-export default Div_3
